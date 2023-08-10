71c5069ff4be3a6dd1d14e2586e1ed5466397f1f
erinnnpark <erinnnpark@gmail.com>
2023-08-10 22:14:35 +0900

    commit test

--- lecture7/quiz7.py
+++ lecture7/quiz7.py
@@ -20,5 +20,7 @@ gender = "남자"
 
 weight = round(std_weight(height /100, gender),2)
 print("키 {0}cm {1}의 표준 체중은 {2}kg 입니다.".format(height, gender, weight))
-    
+
+#정민바보 후후훟 다현 천재 개발자!!!
+
         
